a59a03e957c899d3995d79838112e424e7e8920c
Pedro Zambrano <pedrozambranocajal@gmail.com.com>
2020-09-03 18:21:13 -0300

    init

---
+++ Auto1.h
@@ -0,0 +1,14 @@
+#ifndef AUTO1_H
+#define AUTO1_H
+#include <ctime>
+#include "Auto.h"
+
+class Auto1: public Auto {
+	
+public:
+	Auto1(int v, int x0,int y0);
+	Auto1(){};
+	bool update();		
+};
+
+#endif
